6c10b0898017d0ca077c2ec48eade61147f4a9fe
GKSDeepak <deepakgubbala23@gmail.com>
2024-10-06 17:01:06 +0530

    check server url

--- server/index.js
+++ server/index.js
@@ -83,6 +83,10 @@ app.post('/chat', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('just to see is server is working');
+});
+
 app.get('/try', (req, res) => {
   res.send('Hello, this is some text ayed on the screen!');
 });
